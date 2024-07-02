16ed896ee434e19424aa96e19bb7b89165f17797
Puni Charana <puni@Punis-MacBook-Air.local>
2024-07-02 18:39:03 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'scroll-ng';
+  scrollPositions: number[] = [];
+  items = [
+    {
+      title: 'Top Rated',
+      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    },
+    {
+      title: 'Latest Movies',
+      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    },
+    {
+      title: 'Thriller Movies',
+      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    },
+    {
+      title: 'Romantic Movies',
+      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    },
+    {
+      title: 'Popular Shows',
+      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    },
+    {
+      title: 'Season Special',
+      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    },
+    {
+      title: 'Popular Shows',
+      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    },
+    {
+      title: 'Season Special',
+      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    }
+  ]
+
+  constructor() {
+    this.scrollPositions = new Array(this.items.length).fill(0);
+  }
+
+  next(i: number) {
+    const x = this.items[i].movies.length;
+    if (this.scrollPositions[i] > ((x - 3) * -192)) this.scrollPositions[i] = this.scrollPositions[i] - 192;
+  }
+
+  prev(i: number) {
+    if (this.scrollPositions[i] < 0) this.scrollPositions[i] = this.scrollPositions[i] + 192;
+  }
 }
