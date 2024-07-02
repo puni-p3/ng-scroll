c8b2ef77ebc716a618f60836934b07cfb77c5cb5
Puni Charana <puni@Punis-MacBook-Air.local>
2024-07-02 19:04:37 +0530

    add Orientation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
+enum Orientation {
+  Landscape = "landscape",
+  Portrait = "portrait",
+  Square = "square"
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,35 +16,43 @@ export class AppComponent {
   items = [
     {
       title: 'Top Rated',
-      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+      orientation: Orientation.Square
     },
     {
       title: 'Latest Movies',
-      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+      orientation: Orientation.Landscape
     },
     {
       title: 'Thriller Movies',
-      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+      orientation: Orientation.Portrait
     },
     {
       title: 'Romantic Movies',
-      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+      orientation: Orientation.Landscape
     },
     {
       title: 'Popular Shows',
-      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+      orientation: Orientation.Portrait
     },
     {
       title: 'Season Special',
-      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+      orientation: Orientation.Square
     },
     {
       title: 'Popular Shows',
-      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+      orientation: Orientation.Portrait
     },
     {
       title: 'Season Special',
-      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+      movies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+      orientation: Orientation.Portrait
     }
   ]
 
@@ -47,11 +61,13 @@ export class AppComponent {
   }
 
   next(i: number) {
+    const itemWidth = this.items[i].orientation === Orientation.Landscape ? 288 : 192;
     const x = this.items[i].movies.length;
-    if (this.scrollPositions[i] > ((x - 3) * -192)) this.scrollPositions[i] = this.scrollPositions[i] - 192;
+    if (this.scrollPositions[i] > ((x - 3) * -itemWidth)) this.scrollPositions[i] = this.scrollPositions[i] - itemWidth;
   }
 
   prev(i: number) {
-    if (this.scrollPositions[i] < 0) this.scrollPositions[i] = this.scrollPositions[i] + 192;
+    const itemWidth = this.items[i].orientation === Orientation.Landscape ? 288 : 192;
+    if (this.scrollPositions[i] < 0) this.scrollPositions[i] = this.scrollPositions[i] + itemWidth;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandscapeComponent } from './landscape/landscape.component';
+import { PortraitComponent } from './portrait/portrait.component';
+import { SquareComponent } from './square/square.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandscapeComponent,
+    PortraitComponent,
+    SquareComponent
   ],
   imports: [
     BrowserModule,
